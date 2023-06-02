7a54ef55d4c37b25040f96d7386f2b931e698199
user <jevicntita6@gmail.com>
2023-06-02 14:48:39 +0200

    RapidFood update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <header 
+    <header
       class="bg-dark text-white shadow h-16 justify-end flex items-stretch mx-0 @apply opacity-100 translate-y-0 shadow-[0_10px_30px_0_rgb(33,33,33,0.15)] transition-all duration-[0.3s] ease-[ease-out] px-0 py-2.5;
       -webkit-transition: all 0.3s ease-out fixed-top mb-40
     ">
@@ -19,7 +19,7 @@
       </div>
       <div>
         <router-link :to="{name: 'ingredients'}" class="inline-flex mx-0 items-center px-2 h-full font-semibold hover:bg-gray-400 hover:font-semibold hover:bg-white hover:text-black transition-colors">
-          Meals by Ingredients 
+          Meals by Ingredients
         </router-link>
       </div>
     </header>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,85 @@
 <template>
-  <div>
-    <h1>
-        Welcome to Rapid Food
+  <div class="banner-container">
+    <div class="banner-image"></div>
+    <h1 class="banner-text">
+      Welcome to Rapid Food
     </h1>
+    <h2 class="lead banner-lead">We aim to help you create fancy food descriptions in less time than it would take the average restaurant to serve you drinks.</h2>
+    <div v-if="random.length > 0" class="random-meals">
+      <h3>Random Meals:</h3>
+      <ul>
+        <li v-for="meal in random" :key="meal.idMeal">{{ meal.strMeal }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script setup>
-import { computed, onMounted, ref } from 'vue';
-import store from '../store';
-import axiosClient from '../axiosClient.js'
-import YouTubeButton from '../components/YouTubeButton.vue';
-// import Meals from '../components/Meals.vue';
+<style scoped>
+.banner-container {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+}
 
-const random = ref([])
+.banner-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url("../assets/banner.jpg");
+  background-size: cover;
+  filter: blur(20%);
+}
+
+.banner-text,
+.banner-lead {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: rgb(27, 27, 27);
+}
+
+.banner-text {
+  font-size: 3rem;
+  margin-top: 100px;
+
+}
+
+.banner-lead {
+  font-size: 1.5rem;
+  margin-top: 200px;
+}
+
+.random-meals {
+  margin-top: 40px;
+}
+
+.random-meals h3 {
+  font-size: 1.5rem;
+}
+
+.random-meals ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.random-meals li {
+  margin-bottom: 8px;
+}
+</style>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import axiosClient from '../axiosClient.js';
+
+const random = ref([]);
 
 onMounted(async () => {
-    const response = await axiosClient.get('random.php')
-    console.log(response.data)
-    random.value = response.data
-})
-
+  const response = await axiosClient.get('random.php');
+  random.value = response.data.meals || [];
+});
 </script>
